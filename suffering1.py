7f08faf9a79358ec86280e1d6681cf214ed4a7a4
AdhunaD <adhuna.devineni@gmail.com>
2023-10-07 11:20:11 -0700

    Update suffering1.py

--- suffering1.py
+++ suffering1.py
@@ -4,6 +4,10 @@ def create_a_list_of_numbers(amount_of_numbers):
     for i in range(amount_of_numbers + 1):
         x = random.randint(0, 9999)
         list_of_numbers.append(x)
-    print(list_of_numbers)
+    return list_of_numbers
 
-create_a_list_of_numbers(100)
+mylist = create_a_list_of_numbers(100)
+print(mylist)
+
+max_value = max(mylist)
+print(max_value)
