0c2dbfc0a2e1eaab8983a06d31be8de59446d8bc
atharvgups <atharvgups@gmail.com>
2023-10-14 11:01:28 -0700

    Update suffering1.py
    my suffering has ended

--- suffering1.py
+++ suffering1.py
@@ -9,5 +9,15 @@ def create_a_list_of_numbers(amount_of_numbers):
 mylist = create_a_list_of_numbers(100)
 print(mylist)
 
-max_value = max(mylist)
-print(max_value)
+new_list = [2, 14, 8, 1, 9, 7]
+
+counter = 0
+while counter < len(new_list) - 1:
+    # this  is where I write the logic of the loop
+    if new_list[counter] > new_list[counter+1]:
+        # swap the numbers
+        new_list[counter],new_list[counter+1] = new_list[counter+1],new_list[counter]
+    print(new_list)
+    counter = counter + 1
+
+    # print(new_list)
